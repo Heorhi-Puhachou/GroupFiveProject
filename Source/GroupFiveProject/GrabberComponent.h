d2fd3626dabb5240655664330f0b9ae4fedc6c02
Heorhi-Puhachou <60034257+Heorhi-Puhachou@users.noreply.github.com>
2025-07-20 15:13:29 +0200

    Added GrabberComponent from lesson 14

---
+++ Source/GroupFiveProject/GrabberComponent.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "PhysicsEngine/PhysicsHandleComponent.h"
+
+#include "GrabberComponent.generated.h"
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class GROUPFIVEPROJECT_API UGrabberComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UGrabberComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
+
+	void Interact();
+	void Grab();
+	void Release();
+	void Throw();
+
+private:
+	UPROPERTY(EditAnywhere)
+	float m_GrabbingRange = 200.0f;
+
+	UPROPERTY(EditAnywhere)
+	float m_ImpulseStrenght = 1000.0f;
+
+	UInputComponent *m_InputComponent = nullptr;
+	UPhysicsHandleComponent *m_PhysicsHandle = nullptr;
+};
